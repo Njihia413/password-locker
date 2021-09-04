8461a878bf2a4485d7b7c942a44fd555b28b1914
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-04 16:14:06 +0300

    Add delete_user method to delete users from user list

--- user.py
+++ user.py
@@ -17,4 +17,10 @@ class User :
         '''
         save_user method saves user objects into user_list
         '''
-        User.user_list.append(self)    
+        User.user_list.append(self)  
+
+    def delete_user(self):
+        '''
+        delete_user method deletes user objects from user_list
+        '''
+        User.user_list.remove(self)      
