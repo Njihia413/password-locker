44f3feac178e6ade8c945182bbe010b7880c6b3c
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-04 17:24:24 +0300

    Add account as an object property and instantiate it

--- credentials.py
+++ credentials.py
@@ -3,10 +3,11 @@ class Credentials:
     """
     Class that generates new instances of credentials
     """
-    def __init__(self,user_name,password,email):
+    def __init__(self,account,user_name,password,email):
         '''
         __init__ method helps us to define properties for our objects
         '''
+        self.account = account
         self.user_name = user_name
         self.password = password
         self.email = email
