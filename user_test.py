3c25e2e1f2fc7e83682540aa973b40b76bbd00c0
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-04 15:50:00 +0300

    Test to check if user is successfully saved

--- user_test.py
+++ user_test.py
@@ -8,12 +8,21 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         self.new_user = User("Maureen","Njihia","Njihia413","Melissa@atara413")
 
+    '''
+    Test to check if user is successfully created
+    '''    
     def test_create_user(self):
         self.assertEqual(self.new_user.first_name,"Maureen")
         self.assertEqual(self.new_user.last_name,"Njihia")
         self.assertEqual(self.new_user.locker_user_name,"Njihia413")
         self.assertEqual(self.new_user.locker_password,"Melissa@atara413")
 
+    '''
+    Test to check if the user is saved
+    '''    
+    def test_save_user(self):
+        self.new_user.save_user() #Saving the new user
+        self.assertEqual(len(User.user_list),1)
+
 if __name__ == '__main__':
     unittest.main()
-    
