c4bf393bbc8003668360c0382a0fb72e6aa3a731
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-04 14:54:27 +0300

    Create user class and define object properties

---
+++ user.py
@@ -0,0 +1,9 @@
+class User :
+    """
+    Class that generates new instances of a user
+    """
+    def __init__(self,first_name,last_name,locker_user_name,locker_password):
+        '''
+        __init__ method helps us define properties for our objects
+        '''
+        pass    
