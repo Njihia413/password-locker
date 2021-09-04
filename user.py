352aacf92ce2852dac9aa5b390a53e1394566cc4
Maureen Njihia <maureennjihia468@gmail.com>
2021-09-04 15:05:38 +0300

    Instantiate variables and create an  empty users list array

--- user.py
+++ user.py
@@ -6,4 +6,9 @@ class User :
         '''
         __init__ method helps us define properties for our objects
         '''
-        pass    
+        user_list = [] #Empty users list
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.locker_user_name = locker_user_name
+        self.locker_password = locker_password
